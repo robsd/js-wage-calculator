76fb6bcdb48e517f1c5ebc2b8f37a3a68cc1877a
Robert Dixon <79385125+robsd@users.noreply.github.com>
2022-09-16 15:17:29 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# js-wage-calculator 💰
+
+A JavaScript calculator that converts (roughly) your annual wage to monthly, weekly, daily and hourly values.
+
+## Demo
+
+https://robsd.github.io/js-wage-calculator
---
+++ js/script.js
@@ -0,0 +1,17 @@
+wage = document.getElementById('wage');
+hours = document.getElementById('hours');
+monthly = document.getElementById('monthly');
+weekly = document.getElementById('weekly');
+daily = document.getElementById('daily');
+hourly = document.getElementById('hourly');
+
+function calculate() {
+	monthlyWage = wage.value / 12;
+	weeklyWage = monthlyWage / 4;
+	dailyWage = weeklyWage / 5;
+	
+	monthly.value = monthlyWage;
+	weekly.value = weeklyWage;
+	daily.value = dailyWage;
+	hourly.value = dailyWage / hours.value || 0;
+}
