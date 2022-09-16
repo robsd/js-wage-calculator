5af055048265c0de6becb5cc594f16de06db40b5
Robert Dixon <79385125+robsd@users.noreply.github.com>
2022-09-16 15:55:22 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,10 +8,10 @@ hourly = document.getElementById('hourly');
 function calculate() {
 	monthlyWage = wage.value / 12;
 	weeklyWage = monthlyWage / 4;
-	dailyWage = weeklyWage / 5;
+	dailyWage = weeklyWage / 5
 	
 	monthly.value = monthlyWage;
 	weekly.value = weeklyWage;
 	daily.value = dailyWage;
-	hourly.value = dailyWage / hours.value || 0;
+	hourly.value = dailyWage / hours.value;
 }
